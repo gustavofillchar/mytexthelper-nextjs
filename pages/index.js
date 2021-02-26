e14e19ebc9c6c8b0c8143b3c1e39f386cd83fb27
Gustavo Lima <gustavo.fillchar@gmail.com>
2021-02-26 08:35:04 -0300

    layout mobile fix

--- pages/index.js
+++ pages/index.js
@@ -6,26 +6,21 @@ export default function App() {
   <>
   <Head>
     <meta property="og:type" content="website" />
-
     <meta property="og:title" content="My Text Helper - Convert Case" />
-
     <meta property="og:description" content="A very handy online text tool where you can change between lower case and upper case letters, where you can capitalize, uncapitalize, convert to mix case and transform your text." />
-
     <meta property="og:image" content="https://mytexthelper.jubarte.digital/assets/static/icon.png" />
-
     <meta property="og:url" content="https://mytexthelper.jubarte.digital" />
-
     <meta property="og:site_name" content="My Text Helper"/>
-    <meta name="robots" content="index, follow" />
     <meta name="description" content="A simple and free tool for converting text to UPPERCASE or LOWERCASE. Convert to URL format, encode, decode. Simple and easy to use. Free and online. Converter of texts and words for upper and lower case. Decode that URL full of strange characters into clean text."/>
     <link rel="icon" href="./assets/static/favicon.ico" />
     <link rel="canonical" href="https://mytexthelper.jubarte.digital/" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>My Text Helper | Free tool | Convert Case Helper | Encode Text | Decode Text</title>
+    <title>My Text Helper | Best Free tool | The Best Convert Case Helper Tool | Encode Text | Decode Text | Best Developer Tool</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"/>
   </Head>
 
   <Home/>
+
   </>
   )
   
